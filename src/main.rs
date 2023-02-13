62239c20ec417ddb718bde84f21f92459ce79692
KAYT33N <K80arabi@gmail.com>
2023-02-13 17:13:24 +0330

    made healthcheck route and connection function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused_imports)]
+#[macro_use] extern crate rocket;
+
+use rocket::{
+    get,
+    http::Status,
+    serde::json::Json,
+};
+use::diesel::{
+    prelude::*,
+    pg::PgConnection,
+};
+use dotenvy::dotenv;
+use std::env;
+
+
+#[derive(serde::Serialize)]
+pub struct GenericResponse<T>{
+    pub code: i64,
+    pub status: String,
+    pub response: T,
+}
+
+#[get("/healthcheck")]
+fn healthcheck() -> Json<GenericResponse<String>>{
+    Json(
+        GenericResponse{
+            code: 200,
+            status: "OK".to_string(),
+            response: "".to_string(),
+        }
+    )
+}
+
+fn establish_connection() -> PgConnection {
+    dotenv().ok();
+    let database_url = env::
+        var("DATABASE_URL")
+        .expect("No connection specified");
+    
+    PgConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+}
+
+#[launch]
+fn rocket() -> _ {
+    dotenv().ok();
+    env::set_var("RUST_BACKTRACE",env::var("RUST_BACKTRACE").unwrap());
+    rocket::build()
+        .mount("/",routes![healthcheck])
 }
