d806cba827420b3495289b96f8c740feef1fa90f
KAYT33N <K80arabi@gmail.com>
2023-02-18 17:34:22 +0330

    can create new post

--- src/controllers/post_controller.rs
+++ src/controllers/post_controller.rs
@@ -6,6 +6,7 @@ use rocket::{
 };
 use serde::{
     Serialize,
+    Deserialize,
 };
 use crate::{
     GenericResponse,
@@ -14,6 +15,9 @@ use crate::{
     },
     schema::{
         posts,
+    },
+    guards::{
+        autheduser::AuthedUser,
     }
 };
 use diesel::prelude::*;
@@ -58,6 +62,86 @@ fn posts_show(page: Option<i64>) -> Json<GenericResponse<PostsShowResponses>> {
     })
 }
 
+
+#[derive(Serialize)]
+enum PostsCreateResponses{
+    errors{
+        parent_id: bool,
+        authed: bool,
+        body: bool,
+        unexpected: bool,
+    },
+    post(Post),
+}
+#[derive(Deserialize, Insertable)]
+#[diesel(table_name = posts)]
+struct NewPost{
+    parent_id: Option<i32>,
+    user_id: Option<i32>,
+    body: String,
+}
+#[post("/", data = "<inputs>")]
+fn posts_store(inputs: Json<NewPost>, user: AuthedUser) -> Json<GenericResponse<PostsCreateResponses>>{
+    if inputs.body.len() < 3 {
+        return Json(GenericResponse{
+            code: 422,
+            status: String::from("Unprocessable Entity"),
+            response : PostsCreateResponses::errors{
+                parent_id: false,
+                authed: false,
+                body: true,
+                unexpected: false,
+            },
+        })
+    }
+    let parent_id = inputs.parent_id.unwrap_or(0);
+    if parent_id != 0 {
+        let conn = &mut crate::establish_connection();
+        let flag = !posts::table
+            .find(parent_id)
+            .get_result::<Post>(conn)
+            .is_ok();
+        if flag {
+            return Json(GenericResponse{
+                code: 422,
+                status: String::from("Unprocessable Entity"),
+                response : PostsCreateResponses::errors{
+                    parent_id: true,
+                    authed: false,
+                    body: false,
+                    unexpected: false,
+                },
+            })
+        }
+    }
+    let conn = &mut crate::establish_connection();
+    let result = diesel::
+        insert_into(posts::table)
+        .values(NewPost{
+            parent_id: Some(parent_id),
+            user_id: Some(user.user_id),
+            body: String::from(inputs.body.trim()),
+        })
+        .get_result::<Post>(conn);
+    if !result.is_ok() {
+        return Json(GenericResponse{
+            code: 500,
+            status: String::from("Internal Server Error"),
+            response : PostsCreateResponses::errors{
+                parent_id: false,
+                authed: false,
+                body: false,
+                unexpected: true,
+            },
+        })
+    }
+    return Json(GenericResponse{
+        code: 201,
+        status: String::from("Created"),
+        response : PostsCreateResponses::post(result.unwrap()),
+    })
+}
+
 pub fn routes() -> Vec<Route> {
-    routes![posts_show]
+    routes![posts_show, posts_store]
 }
