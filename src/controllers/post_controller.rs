41e821d6cb6288d6d59ab1392ace852f1f2da346
KAYT33N <K80arabi@gmail.com>
2023-02-25 23:23:23 +0330

    added a route to get a thread

--- src/controllers/post_controller.rs
+++ src/controllers/post_controller.rs
@@ -20,7 +20,15 @@ use crate::{
         autheduser::AuthedUser,
     }
 };
-use diesel::prelude::*;
+use diesel::{
+    prelude::*,
+    sql_types::{
+        Integer,
+        Text,
+        Timestamp,
+        BigInt,
+    },
+};
 
 
 #[derive(Serialize)]
@@ -64,6 +72,66 @@ fn posts_show_by_date(page: Option<i64>, len: Option<i64>) -> Json<GenericRespon
         response: PostsShowResponses::posts(results)
     })
 }
+
+
+#[derive(Serialize, QueryableByName)]
+struct RespondedPost{
+    #[diesel(sql_type = Integer)]
+    id: i32,
+    #[diesel(sql_type = Integer)]
+    parent_id: i32,
+    #[diesel(sql_type = Integer)]
+    user_id: i32,
+    #[diesel(sql_type = Text)]
+    body: String,
+    #[diesel(sql_type = Timestamp)]
+    created_at: chrono::NaiveDateTime,
+    #[diesel(sql_type = BigInt)]
+    replies: i64,
+}
+#[derive(Serialize)]
+enum PostFetchResponses{
+    errors{
+        post_id: bool,
+        unexpected: bool,
+    },
+    posts(Vec<RespondedPost>),
+}
+#[get("/<post_id>")]
+fn posts_show_by_thread(post_id: i32) -> Json<GenericResponse<PostFetchResponses>> {
+    let conn = &mut crate::establish_connection();
+    let query = String::from(format!("select id, parent_id, user_id,  body, created_at, (with recursive comms as ( select id, parent_id from posts where id = a.id union all select c.id, c.parent_id from comms join posts c on c.parent_id = comms.id ) select count(id)-1 from comms  ) as replies from posts a where a.parent_id = {0} OR a.id = {0}", post_id));
+    let results = diesel::
+        sql_query(&query)
+        .get_results::<RespondedPost>(conn);
+    if !results.is_ok() {
+        return Json(GenericResponse{
+            code: 500,
+            status: String::from("Internal server err"),
+            response: PostFetchResponses::errors{
+                post_id: false,
+                unexpected: true,
+            }
+        })
+    }
+    let results = results.unwrap() ;
+    if results.len() == 0 {
+        return Json(GenericResponse{
+            code: 404,
+            status: String::from("Not Found"),
+            response: PostFetchResponses::errors{
+                post_id: true,
+                unexpected: false,
+            }
+        })
+    }
+    Json(GenericResponse{
+        code: 200,
+        status: String::from("Ok"),
+        response: PostFetchResponses::posts(results)
+    })
+}
+
 
 
 #[derive(Serialize)]
@@ -149,5 +217,6 @@ pub fn routes() -> Vec<Route> {
     routes![
         posts_show_by_date, 
         posts_store, 
+        posts_show_by_thread
         ]
 }
